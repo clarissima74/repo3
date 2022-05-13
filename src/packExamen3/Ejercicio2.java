e10bc2ad8586b6bcd551ac0d56563b98d4a1546e
'clara <clara.catala@iessampedro.org>
2022-05-13 10:19:17 +0200

    Segunda versión

--- src/packExamen3/Ejercicio2.java
+++ src/packExamen3/Ejercicio2.java
@@ -1,14 +1,10 @@
 package packExamen3;
 
-public class Ejercicio1 {
+public class Ejercicio2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-	}
-	public static void Mensaje()
-	{
-		System.out.println("Hola");
 	}
 
 }
