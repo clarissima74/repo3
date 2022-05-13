1168397780f32fc1704b3ea48a41ed424282b4f4
'clara <clara.catala@iessampedro.org>
2022-05-13 10:24:12 +0200

    Primera del ejercicio 3

---
+++ src/packExamen3/Ejercicio3.java
@@ -0,0 +1,14 @@
+package packExamen3;
+
+public class Ejercicio3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	protected static void Saludo3()
+	{
+		System.out.println("Saludando");
+	}
+
+}
